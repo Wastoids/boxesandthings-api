30200e30773ba3efd2a6323b7449aa05aef5c65a
ashwinrrao <rashwinrao1993@gmail.com>
2021-04-02 07:21:31 -0400

    add a service layer to the code

---
+++ service/box.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"errors"
+
+	"github.com/Wastoids/boxesandthings-api/model"
+)
+
+var (
+	errInvalidUser = errors.New("invalid user")
+)
+
+type Storage interface {
+	GetTopLevelBoxesForUser(userID string) ([]model.Box, error)
+}
+
+type Box struct {
+	db Storage
+}
+
+func (b Box) GetTopLevelBoxesForUser(userID string) ([]model.Box, error) {
+	if len(userID) == 0 {
+		return nil, errInvalidUser
+	}
+	return b.db.GetTopLevelBoxesForUser(userID)
+}
+
+func NewBoxService() Box {
+	return Box{}
+}
