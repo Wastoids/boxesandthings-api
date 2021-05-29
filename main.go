96558384ae8a8be2ded146c23b625c8240c2bd22
ashwinrrao <rashwinrao1993@gmail.com>
2021-05-29 11:35:22 -0400

    return a proxy event on error

--- main.go
+++ main.go
@@ -13,7 +13,22 @@ func main() {
 func HandleRequest(event events.APIGatewayProxyRequest) (interface{}, error) {
 	f, err := controller.NewController().GetFunction(event)
 	if err != nil {
-		panic("invalid resource path")
+		return getError(400, err), nil
 	}
-	return f.Run()
+	response, err := f.Run()
+	if err != nil {
+		return getError(400, err), nil
+	}
+	return response, nil
+}
+
+func getError(statusCode int, err error) events.APIGatewayProxyResponse {
+	return events.APIGatewayProxyResponse{
+		StatusCode: statusCode,
+		Headers: map[string]string{
+			"content-type": "application/json",
+		},
+		Body:            err.Error(),
+		IsBase64Encoded: false,
+	}
 }
